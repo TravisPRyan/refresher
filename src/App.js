8a855c2b557d4be99c0b69671e5406891c466671
TravisPRyan <TravisPRyan@protonmail.com>
2020-03-06 14:21:07 -0500

    v2 Basic Framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import GoalList from "./components/GoalList";
 
-function App() {
+const App = () => {
+  const myGoals = [
+    { id: "g1", text: "Finish Portfolio" },
+    { id: "g2", text: "Learn Graphql" },
+    { id: "g3", text: "Work on non-profit" }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="goals">
+      <h2>Goals</h2>
+      <GoalList goals={myGoals} />
     </div>
   );
-}
+};
 
 export default App;
