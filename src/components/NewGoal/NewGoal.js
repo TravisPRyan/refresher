486358872ebbb98ff01ddd884c9027b2f1cc4ca2
TravisPRyan <TravisPRyan@protonmail.com>
2020-03-10 17:22:10 -0400

    v4 State Cont... Fetching Inputs

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewGoal = props => {
+  const [userInput, setUserInput] = useState("");
   const addGoalHandler = event => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: "A new Goal has arrived"
+      text: userInput
     };
+
+    setUserInput("");
 
     props.onAddGoal(newGoal);
   };
 
+  const inputChangeHandler = event => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <form className="newGoal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={userInput} onChange={inputChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
